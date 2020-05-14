10d16c3afb6237112f83108a069a6570543c4df4
ruitaiS <shaoruitai@gmail.com>
2020-05-13 20:30:51 -0400

    working on game code

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,31 @@
-function say(message){
-    document.getElementById("message").innerHTML = message;
-}
+document.getElementById("message").innerHTML = "yo";
+
+
+
+function say(msg){
+    document.getElementById("message").innerHTML = msg;
+};
 
 say("hi");
 
 function hey(){
     say("Hey! Stay on Your Side!");
-}
+};
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
+};
 
 function press(user){
 
     let ai = getRndInteger(1,3);
-    if user==ai{
-        say("Tie!");
 
-    }else if (user-ai == -2) || (user-ai == 1){
+
+    if (user==ai){
+        say("Tie!");
+    }else if ((user-ai == -2) || (user-ai == 1)){
         say("Win!");
     }else{
         say("Lose!");
-    }
-}
+    };
+};
