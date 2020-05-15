a7003ab1e506caf975dfa70dd0f6bb5c0e459fbf
ruitaiS <shaoruitai@gmail.com>
2020-05-14 23:37:15 -0400

    icon behavior fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let myScore = 0;
 let yourScore = 0;
+let infoScreen = false;
 
 
 
@@ -10,15 +11,24 @@ function say(msg){
 say("How about a game?");
 
 function hey(){
-    
-    //timeout function doesn't seem to want to work
-    window.setTimeout(say("hmm"), 3000);
     say("Hey! Stay on Your Side!");
+    
+    //timeout doesn't seem to work if you pass a variable to the inner function
+    //setTimeout(reset, 3000);
 
 };
 
 function about(){
-    say("It's rock paper scissors! What more is there to say??\n \nIcons by Cristiano Zouacs from the Noun Project.\n \nColor scheme by Penelope Fung.");
+    if (!infoScreen){
+    infoScreen = true;
+    say("It's rock paper scissors! What more is there to say??\nIcons by Cristiano Zouacs from the Noun Project.\nColor scheme by Penelope Fung.");
+    document.getElementById("aboutBtn").innerHTML = "Back";
+    }else{
+    infoScreen = false;
+    document.getElementById("aboutBtn").innerHTML = "About";
+    say("Let's Play!");
+    
+    };
 };
 
 function resetIcons(){
@@ -49,7 +59,6 @@ function getRndInteger(min, max) {
 
 function decodeMove(n, whose){
     
-    resetIcons();
     if (n == 1){
         document.getElementById(whose+"Rock").src="./img/whiterock.png";
         return "Rock";
@@ -64,6 +73,7 @@ function decodeMove(n, whose){
 
 function press(user){
 
+    resetIcons();
     let ai = getRndInteger(1,3);
 
 
@@ -73,7 +83,7 @@ function press(user){
         say(decodeMove(user, "your") +" beats " + decodeMove(ai, "my") + "!\n You Win!");
         yourScore += 1;
     }else{
-        say(decodeMove(ai, "your") + " beats " + decodeMove(user, "my") + "!\n You Lose!");
+        say(decodeMove(ai, "my") + " beats " + decodeMove(user, "your") + "!\n You Lose!");
         myScore += 1;
     };
 
