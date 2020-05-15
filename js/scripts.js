e1fd053b3d9eeeb24ec854ab54d3cac2fd1c6201
ruitaiS <shaoruitai@gmail.com>
2020-05-15 01:09:36 -0400

    All done! Final Commit!

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 let myScore = 0;
 let yourScore = 0;
 let infoScreen = false;
+let T1; let T2;
 
 
 
@@ -11,14 +12,18 @@ function say(msg){
 say("How about a game?");
 
 function hey(){
+    resetIcons();
     say("Hey! Stay on Your Side!");
     
     //timeout for whatever reason passes the third parameter to the callback
-    setTimeout(say, 3000, "");
+    T1 = setTimeout(say, 1250, "");
 
 };
 
 function about(){
+    clearTimeout(T1);
+    clearTimeout(T2);
+
     if (!infoScreen){
     infoScreen = true;
     say("It's rock paper scissors! What more is there to say??\nIcons by Cristiano Zouacs from the Noun Project.\nColor scheme by Penelope Fung.");
@@ -45,6 +50,8 @@ document.getElementById("yourScissors").src="./img/scissors.png";
 }
 
 function reset(){
+    clearTimeout(T1);
+    clearTimeout(T2);
     resetIcons();
     myScore = 0;
     yourScore = 0;
@@ -52,8 +59,8 @@ function reset(){
     document.getElementById("yourscore").innerHTML = "";
 
     say("Beep boop.");
-    setTimeout(say, 750, "");
-    setTimeout(say, 2750, "Another game?");
+    T1 = setTimeout(say, 750, "");
+    T2 = setTimeout(say, 2750, "Another game?");
 };
 
 function getRndInteger(min, max) {
@@ -75,6 +82,8 @@ function decodeMove(n, whose){
 };
 
 function press(user){
+    clearTimeout(T1);
+    clearTimeout(T2);
 
     resetIcons();
     let ai = getRndInteger(1,3);
@@ -92,4 +101,7 @@ function press(user){
 
     document.getElementById("myscore").innerHTML = "Me: " + myScore;
     document.getElementById("yourscore").innerHTML = "You: " + yourScore;
+
+    T1 = setTimeout(say, 1250, ""); 
+    T1 = setTimeout(resetIcons, 1250);   
 };
